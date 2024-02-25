759bcb392d10b6b29a9010391e5955e6efd5d963
username547 <ermolenkonsk@mail.ru>
2024-02-25 14:24:32 +0700

    temp models

--- Model/Data/ApplicationDbContext.cs
+++ Model/Data/ApplicationDbContext.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Ozon.Model.Data
 {
-    class ApplicationDbContext
+    public class ApplicationDbContext: DbContext
     {
+        public DbSet<User> Users {  get; set; }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ANDREW;Initial Catalog=Ozon;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
     }
 }
